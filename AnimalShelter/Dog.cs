8874f5fce67d6d30b86e0d7e919f39670bd91d43
andy-coding <32386192+andy-coding@users.noreply.github.com>
2017-09-29 12:29:38 +0100

    Add project files.

---
+++ AnimalShelter/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter
+{
+    public class Dog
+    {
+        private int age;
+
+        public string Name;
+
+        public Dog(string name, int age)
+        {
+            this.Name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ AnimalShelter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter
+{
+    class Program
+    {
+        static void Main()
+        {
+            AnimalShelter dogsShelter = new AnimalShelter(10);
+            Dog dog1 = new Dog("Jackie", 1);
+            Dog dog2 = new Dog("Lassy", 2);
+            Dog dog3 = new Dog("Rex", 3);
+            dogsShelter.Shelter(dog1);
+            dogsShelter.Shelter(dog2);
+            dogsShelter.Shelter(dog3);
+            dogsShelter.Release(1);
+            Console.WriteLine("Dog: {0} released.", dog2.Name); // Releasing dog2
+        }
+    }
+}
+
